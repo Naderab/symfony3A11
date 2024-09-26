a6d360dbe655bdc0ff6464437550b230f0b81c8a
Naderab <abdellaouinader@gmail.com>
2024-09-26 11:22:35 +0100

    Séance 3 TWIG

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class StudentController extends AbstractController{
+
+    
+    public function SayHello(){
+        $msg = 'Hello 3A11';
+        $student = array('id'=>1,'name'=>'Flen');
+        $students = array(
+            array('id'=>1,'name'=>'Flen','moyenne'=>14,'image'=>'images/user1.jpg'),
+            array('id'=>2,'name'=>'Ben Flen','moyenne'=>19,'image'=>'images/user2.png'),
+            array('id'=>3,'name'=>'falten','moyenne'=>8,'image'=>'images/user3.png')
+        );
+        $tab = [1,2,3,4];
+        // return new Response("Bonjour 3A11");
+        return $this->render('test.html.twig',[
+            'm'=>$msg,
+            's'=>$student,
+            'students'=>$students,
+            'tab'=>$tab
+        ]);
+    }
+
+    public function test(){
+        return new Response("test test");
+    }
+
+    //Route(path,le nom de route)
+    #[Route("/pathtest2",name:"app_test2")] 
+    public function test2(){
+        return new Response("test route méthode 2");
+    }
+
+    #[Route("/redirect",name:"app_redirect")] 
+    public function testRedirect(){
+        return $this->redirectToRoute('app_test2'); #POO
+    }
+
+
+    #[Route("/user/{id}/{name}",name:'app_testroutepara')]
+    public function testRoutePara($id,$name){
+        return new Response("test : ".$id."Name : ".$name);
+    }
+
+
+
+}
+
+?>
